fa7ab663d1323cdc3003b3504765bde1bc857328
Artesonraju <etienne.molto@protonmail.com>
2018-09-14 03:15:41 +0200

    update release preparation

--- prepare-release.sh
+++ prepare-release.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+
 rm allalin.zip
 rm -rf ./docs
-
 lein do clean, fig:min
-
-cd ./resources/public/
-zip -r ../../allalin.zip ./*
-cd ../..
-
-cp -r ./resources/public/. ./docs
+cp -r ./resources/public/. ./dist
+cp ./target/public/cljs-out/dev-main.js ./dist/min.js
+cd ./dist
+sed -i -e 's|cljs-out/dev-main.js|min.js|g' ./index.html
+rm test.html
+zip -r ../allalin.zip ./*
+cd ..
+cp -r ./dist ./docs
 sed -i -e 's/; :disable-reload true/:disable-reload true/g' ./docs/config.edn
+rm -r ./dist
